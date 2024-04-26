c11e20f27327e6028893122a239648253b125947
Yashbabani0 <yashbabani09@gmail.com>
2024-04-26 22:11:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const ratingsBtn = document.querySelectorAll(".ratings");
+const submitBtn = document.querySelector("#submit-btn");
+const showRating = document.querySelector("span");
+const ratingButton1 = document.querySelector(".rating1");
+const ratingButton2 = document.querySelector(".rating2");
+const ratingButton3 = document.querySelector(".rating3");
+const ratingButton4 = document.querySelector(".rating4");
+const ratingButton5 = document.querySelector(".rating5");
+
+
